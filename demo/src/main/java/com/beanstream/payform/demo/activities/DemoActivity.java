214bf70fe23d5377a5f531ed8b70befae4b5ddb7
David Light <dlight@beanstream.com>
2016-08-05 11:00:02 -0700

    MDA-644 Implemented demo app with button and text

---
+++ demo/src/main/java/com/beanstream/payform/demo/activities/DemoActivity.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016 Beanstream Internet Commerce, Inc. All rights reserved.
+ *
+ * Created by dlight on 2016-08-04.
+ */
+
+package com.beanstream.payform.demo.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.beanstream.payform.demo.R;
+
+
+public class DemoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_demo);
+    }
+}
