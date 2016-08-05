9bb82946fc3e42c7c77302f3c2105a158f9b91a8
David Light <dlight@beanstream.com>
2016-08-05 11:38:44 -0700

    MDA-644 Set token visibility to button click

--- demo/src/main/java/com/beanstream/payform/demo/activities/DemoActivity.java
+++ demo/src/main/java/com/beanstream/payform/demo/activities/DemoActivity.java
@@ -8,13 +8,16 @@ package com.beanstream.payform.demo.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.beanstream.payform.demo.R;
 
 
-public class DemoActivity extends Activity {
+public class DemoActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +27,15 @@ public class DemoActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_demo);
+
+        final Button button = (Button) findViewById(R.id.demo_pay_button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView tokenText = (TextView)findViewById(R.id.demo_pay_token);
+        tokenText.setText("Token: new token");
+        tokenText.setVisibility(View.VISIBLE);
     }
 }
