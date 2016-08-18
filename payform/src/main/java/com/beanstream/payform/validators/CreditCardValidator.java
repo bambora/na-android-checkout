5a680d94b2fa6cda279488d8d5ed151fa8599c4f
David Light <dlight@beanstream.com>
2016-08-17 17:06:08 -0700

    MDA-645 Add luhn validation

--- payform/src/main/java/com/beanstream/payform/validators/CreditCardValidator.java
+++ payform/src/main/java/com/beanstream/payform/validators/CreditCardValidator.java
@@ -5,7 +5,6 @@
 package com.beanstream.payform.validators;
 
 import android.text.TextUtils;
-import android.util.Log;
 
 import com.beanstream.payform.models.CardType;
 import com.beanstream.payform.models.CreditCard;
@@ -21,14 +20,15 @@ public class CreditCardValidator {
         }
         String cardNumber = card.getCardNumber();
         String cardType = card.getCardType();
-        return isValidCardType(cardNumber) &&
-                isValidCardNumber(cardNumber, cardType) &&
-                isValidCvv(card.getCvv(), cardType) &&
-                isValidLuhn(cardNumber);
+        return isValidCardType(cardNumber)
+                && isValidCardNumber(cardNumber, cardType)
+                && isValidCvv(card.getCvv(), cardType)
+                && isValidLuhn(cardNumber)
+                ;
     }
 
     public static boolean isValidCardNumber(String cardNumber, String cardType) {
-        if (TextUtils.isEmpty(cardNumber)) {
+        if ((cardNumber == null) || (TextUtils.isEmpty(cardNumber.trim()))) {
             return false;
         }
         cardNumber = cardNumber.replace(" ", "");
@@ -36,8 +36,7 @@ public class CreditCardValidator {
     }
 
     public static boolean isValidCardType(String cardNumber) {
-        Log.d("isValidCardType","cardNumber");
-        if (TextUtils.isEmpty(cardNumber)) {
+        if ((cardNumber == null) || (TextUtils.isEmpty(cardNumber.trim()))) {
             return false;
         }
         String cardType = CardType.getCardTypeFromCardNumber(cardNumber);
@@ -45,13 +44,32 @@ public class CreditCardValidator {
     }
 
     public static boolean isValidCvv(String cvv, String cardType) {
-        if (TextUtils.isEmpty(cvv)) {
+        if ((cvv == null) || (TextUtils.isEmpty(cvv.trim()))) {
             return false;
         }
         return cvv.length() == CardType.getCvvLengthForCardType(cardType);
     }
 
     public static boolean isValidLuhn(String cardNumber) {
-        return false;
+
+        if ((cardNumber == null) || (TextUtils.isEmpty(cardNumber.trim()))) {
+            return false;
+        }
+        cardNumber = (cardNumber + "").replace(" ", "");
+
+        int sum = 0;
+        boolean alternate = false;
+        for (int i = cardNumber.length() - 1; i >= 0; i--) {
+            int n = Integer.parseInt(cardNumber.substring(i, i + 1));
+            if (alternate) {
+                n *= 2;
+                if (n > 9) {
+                    n = (n % 10) + 1;
+                }
+            }
+            sum += n;
+            alternate = !alternate;
+        }
+        return (sum % 10 == 0);
     }
 }
