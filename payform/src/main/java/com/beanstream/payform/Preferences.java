3363a5234cd1e629833ae6b6e2a209bc3e12b026
David Light <dlight@beanstream.com>
2016-09-06 16:42:54 -0700

    MDA-649 Project changes

--- payform/src/main/java/com/beanstream/payform/Preferences.java
+++ payform/src/main/java/com/beanstream/payform/Preferences.java
@@ -11,11 +11,14 @@ import android.content.SharedPreferences;
  * Created by dlight on 2016-08-19.
  */
 public class Preferences {
+    public static String CardType = "cardType";
+    public static String TokenRequestTimeoutInSeconds = "tokenRequestTimeoutInSeconds";
     private static Preferences preferences;
     private SharedPreferences sharedPreferences;
 
-    public static String CardType = "cardType";
-    public static String TokenRequestTimeoutInSeconds = "tokenRequestTimeoutInSeconds";
+    private Preferences(Context context) {
+        sharedPreferences = context.getSharedPreferences("com.beanstream.payform", Context.MODE_PRIVATE);
+    }
 
     public static Preferences getInstance(Context context) {
         if (preferences == null) {
@@ -24,18 +27,14 @@ public class Preferences {
         return preferences;
     }
 
-    private Preferences(Context context) {
-        sharedPreferences = context.getSharedPreferences("com.beanstream.payform",Context.MODE_PRIVATE);
-    }
-
     public void saveData(String key, String value) {
         SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
-        prefsEditor .putString(key, value);
+        prefsEditor.putString(key, value);
         prefsEditor.commit();
     }
 
     public String getData(String key) {
-        if (sharedPreferences!= null) {
+        if (sharedPreferences != null) {
             return sharedPreferences.getString(key, "");
         }
         return "";
